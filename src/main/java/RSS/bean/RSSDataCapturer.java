0c2e7fc7f5214c58d403b3f5efa2040d5c32e41c
you0017 <2092425375@qq.com>
2024-08-04 11:21:46 +0800

    自定义RSS

--- src/main/java/RSS/bean/RSSDataCapturer.java
+++ src/main/java/RSS/bean/RSSDataCapturer.java
@@ -5,10 +5,8 @@ import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
 import org.xml.sax.helpers.DefaultHandler;
 
-import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
-import java.io.IOException;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
--- src/main/java/RSS/writer/NewsWriterTask.java
+++ src/main/java/RSS/writer/NewsWriterTask.java
@@ -6,7 +6,6 @@ import RSS.dao.ProjectProperties;
 import org.apache.log4j.Logger;
 
 import java.io.BufferedWriter;
-import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
